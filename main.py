cbdc6eda26348ff1a10ba8a28af47e1dc75a5f8e
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-04-12 17:29:55 +0700

    feat: register vote routes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import auth, user, user_dashboard, decision
+from routers import auth, user, user_dashboard, decision, vote
 from core.config import settings
 
 app = FastAPI(debug=settings.DEBUG)
@@ -8,6 +8,7 @@ app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(user_dashboard.router)
 app.include_router(decision.router)
+app.include_router(vote.router)
 
 @app.get("/")
 def read_root():
